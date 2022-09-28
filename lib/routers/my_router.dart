bf2b934b2b12df25767ecde921211a75d1f3ecc8
lenovo <lavish7939@gmail.com>
2022-09-28 10:07:36 +0530

    28/9/22

--- lib/routers/my_router.dart
+++ lib/routers/my_router.dart
@@ -57,9 +57,8 @@ class MyRouter {
   static var helpAndSupportScreen = "/helpAndSupportScreen";
 
   static var route = [
-    GetPage(name: '/', page: () => const BottomNavbar()),
-    GetPage(
-        name: MyRouter.onBoardingScreen, page: () => const OnBoardingScreen()),
+    GetPage(name: '/', page: () => const SplashScreen()),
+    GetPage(name: MyRouter.onBoardingScreen, page: () => const OnBoardingScreen()),
     GetPage(name: MyRouter.signUpScreen, page: () => const SignUpScreen()),
     GetPage(name: MyRouter.loginScreen, page: () => const LoginScreen()),
     GetPage(name: MyRouter.bottomNavbar, page: () => const BottomNavbar()),
